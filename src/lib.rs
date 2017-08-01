fe2b6d14ac57eb55412c253971cb3750a91007e0
Bram van den Heuvel <b.vandenheuvel@student.tudelft.nl>
2017-08-01 21:36:40 +0200

    Panic instead of evaluating to Result values

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,5 @@
 use std::collections::HashMap;
-use std::error::Error;
-use std::fmt;
 use std::hash::Hash;
-
-type OperationResult<'a, T> = Result<T, OperationError<'a>>;
-
-#[derive(Debug)]
-pub struct OperationError<'a> {
-    message: &'a str,
-}
-
-impl<'a> Error for OperationError<'a> {
-    fn description(&self) -> &str {
-        self.message
-    }
-
-    fn cause(&self) -> Option<&Error> {
-        None
-    }
-}
-
-impl<'a> fmt::Display for OperationError<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
 
 pub struct BiMap<T, U> {
     left_to_right: HashMap<T, U>,
@@ -38,15 +13,10 @@ impl<T: Copy + Eq + Hash, U: Copy + Eq + Hash> BiMap<T, U> {
             right_to_left: HashMap::new(),
         }
     }
-    pub fn insert<'a>(&mut self, l: T, r: U) -> OperationResult<'a, (T, U)> {
-        if self.left_to_right.get(&l).is_some() && self.left_to_right.get(&l).unwrap() != &r {
-            return Err(OperationError {
-                message: "Collision with existing entry",
-            });
-        }
+    pub fn insert(&mut self, l: T, r: U) {
+        assert!(self.left_to_right.get(&l).is_none() || self.left_to_right.get(&l).unwrap() == &r);
         self.left_to_right.insert(l, r);
         self.right_to_left.insert(r, l);
-        Ok((l, r))
     }
     pub fn size(&self) -> usize {
         self.left_to_right.len()
@@ -68,9 +38,8 @@ mod tests {
     #[test]
     fn insert_single() {
         let mut map: BiMap<&str, &str> = BiMap::new();
-        let result = map.insert("abc", "xyz");
+        map.insert("abc", "xyz");
 
-        assert!(result.is_ok());
         assert_eq!(map.size(), 1);
     }
 
@@ -78,18 +47,16 @@ mod tests {
     fn insert_repeat() {
         let mut map: BiMap<&str, &str> = BiMap::new();
         map.insert("abc", "xyz");
-        let result = map.insert("abc", "xyz");
-        assert!(result.is_ok());
+        map.insert("abc", "xyz");
+
         assert_eq!(map.size(), 1);
     }
 
     #[test]
+    #[should_panic]
     fn insert_other_value() {
         let mut map: BiMap<&str, &str> = BiMap::new();
         map.insert("abc", "xyz");
-        let result = map.insert("abc", "123");
-
-        assert!(result.is_err());
-        assert_eq!(map.size(), 1);
+        map.insert("abc", "123");
     }
 }
